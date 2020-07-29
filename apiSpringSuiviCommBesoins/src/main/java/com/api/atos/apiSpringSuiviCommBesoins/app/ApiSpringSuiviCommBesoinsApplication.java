04250ea7bafc6b22936ec2b5ca7fafef321feb60
DUMORTIER Christophe <christophedemetz57@gmail.com>
2020-07-29 20:06:38 +0200

    commit du backend version 0

---
+++ apiSpringSuiviCommBesoins/src/main/java/com/api/atos/apiSpringSuiviCommBesoins/app/ApiSpringSuiviCommBesoinsApplication.java
@@ -0,0 +1,13 @@
+package com.api.atos.apiSpringSuiviCommBesoins.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSpringSuiviCommBesoinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiSpringSuiviCommBesoinsApplication.class, args);
+	}
+
+}
---
+++ apiSpringSuiviCommBesoins/src/test/java/com/api/atos/apiSpringAngularSuivicombdd/ApiSpringAngularSuivicombddApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.atos.apiSpringAngularSuivicombdd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiSpringAngularSuivicombddApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
